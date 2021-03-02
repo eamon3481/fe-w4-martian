851fd0c837516ca06d7876d8ecdeaa40f5bfc65e
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-03-02 16:40:44 +0900

    [TEST]Make MyPromise class

---
+++ MyPromise.js
@@ -0,0 +1,23 @@
+class MyPromise {
+  constructor(initialStr) {
+    this.cbList = [];
+
+    setTimeout(() => {
+      this.cbList.reduce((acc, func) => func(acc), initialStr);
+    }, 100);
+  }
+
+  then(cb) {
+    this.cbList.push(cb);
+    return this;
+  }
+}
+
+console.log("start");
+
+new MyPromise("hello")
+  .then((v) => v + " world")
+  .then((v) => v + " and crong")
+  .then((v) => console.log(v));
+
+console.log("end");
