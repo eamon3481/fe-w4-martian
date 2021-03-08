b0a29d7ab52827c45ba3fa342cd834b66de407c7
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-03-09 00:49:59 +0900

    [STYLE]Syling input

--- test.js
+++ test.js
@@ -1,80 +1,78 @@
-function delay(code) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log(code);
-      resolve();
-    }, 2000);
-  });
-}
-
-const array = [
-  ["A", "0"],
-  ["c", "3"],
-  ["1", "3"],
-];
-
 function test() {
-  const promiseFunction = () =>
-    new Promise((resolve) =>
-      setTimeout(() => {
-        console.log("w");
-        resolve("result");
-      }, 1000)
-    );
-
   const array = [
     ["A", "0"],
     ["c", "3"],
     ["1", "3"],
   ];
   // (1) test를 async로 감싸는 대신, for문을 async 즉시실행함수로 감싸도 된다
-
-  const cd = () => {
-    for (let i = 0, p = Promise.resolve(); i < 2; j++) {
-      p = p // --- ⑤ // --- ⑥
-        .then(() => {
-          setTimeout(() => {
-            console.log("w");
-            resolve("result");
-          }, 1000);
-        });
-    }
+  const delay = (array) => {
+    return new Promise((res) =>
+      setTimeout(() => {
+        for (let j = 0, f = Promise.resolve(); j < array.length; j++) {
+          f = f.then(
+            (_) =>
+              new Promise((resolve) =>
+                setTimeout(function () {
+                  console.log(array[j]);
+                  resolve();
+                }, 1000)
+              )
+          );
+        }
+        res();
+      }, 1000)
+    );
   };
 
-  for (let j = 0, pending = Promise.resolve(); j < 3; j++) {
-    // --- ①
-    pending = pending // --- ⑤ // --- ⑥
-      .then(() => {
-        // --- ②
-        return new Promise((resolve) => {
-          resolve(()=> cd()); // --- ③
-        });
-      });
-    console.log("pending", pending); //pending 값도 체크해보자.
+  for (let i = 0, p = Promise.resolve(); i < array.length; i++) {
+    p = p
+      .then(
+        (_) =>
+          new Promise((resolve) =>
+            setTimeout(function () {
+              resolve(array[i]);
+            }, 1000)
+          )
+      )
+      .then((array) => delay(array));
   }
-
-  /*
-    (async () => {
-      	// (2) forEach 대신 for ... of를 사용한다
-        for(let i=0; i<3; i++){
-        for (let element of array[i]) {
-            const result = await promiseFunction();
-            console.log(element);
-        }
-        console.log(" ");
-    }
-    })();
-
 }
-*/
-}
-test();
-
 /*
-arr.reduce(async (prevProm, item) => {
-  await prevProm;
-  return prevProm.then(item);
-}, Promise.resolve());
+[1, 2, 3, 4, 5, 6, 7].reduce(
+  (pending, curr) =>
+    pending.then(() => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log(curr);
+          resolve();
+        }, 1000);
+      });
+    }),
+  Promise.resolve()
+);*/
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+let p = Promise.resolve();
+for (let i of array) {
+  p = p.then(
+    (_) =>
+      new Promise((resolve) =>
+        setTimeout(function () {
+          console.log(i);
+          resolve();
+        }, 1000)
+      )
+  );
+}
 
 
-*/
+(async function foo() {
+ for (let v of data) {
+    await new Promise( (resolve) => {
+        setTimeout(()=> {
+           resolve(v);
+        }, 2000);
+   })
+   console.log(v);
+ }
+})();
